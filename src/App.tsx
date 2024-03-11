bc977e9bcb4100d043f2296711abd4d2f984c747
FLEXISAF <mubarak.ibrahim@flexisaf.com>
2024-03-11 13:56:49 +0100

    did some conversions for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import LandinPage from './pages/LandinPage/LandinPage';
 import { ConfigProvider } from 'antd';
 import Register from './pages/Forms/Register';
 import Invoice from './pages/Invoice/Invoice';
+import DashboardLayout from './pages/Dashboard/DashboardLayout';
+import DashboardHome from './pages/Dashboard/DashboardHome';
 
 function App() {
   return (
@@ -22,6 +24,9 @@ function App() {
           <Route path="/" element={<LandinPage />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Invoice" element={<Invoice />} />
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<DashboardHome />} />
+          </Route>
         </Routes>
       </ConfigProvider>
     </BrowserRouter>
---
+++ src/pages/Dashboard/DashboardLayout.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from 'react-router-dom';
+import DashbordIcon from './compopnents/DashbordIcon';
+
+export default function DashboardLayout() {
+  return (
+    <div className="flex h-[100vh] w-full bg-white">
+      <div className="w-0 md:w-[15%] h-full bg-[#F2F2F2] py-20 px-5">
+        <h1 className="text-[12px] md:text-[14px] font-bold text-center mb-32">
+          🇳🇬 NIGERIA-MOROCCO 🇲🇦
+        </h1>
+        <div className="w-full h-[40px] bg-lightGreen flex items-center px-3 rounded-md justify-center">
+          <DashbordIcon />
+          <h1 className="text-[14px] text-white ml-3">Dashboard</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-col flex-1">
+        <div className="bg-white h-[100px] w-full flex items-center justify-end px-10">
+          <div className="flex items-center">
+            <img src="/start.png" className="w-[40px] h-[40px] rounded-full" />
+            <div className=" ml-5">
+              <h1 className="text-[14px] text-[#4C4C4C]">Mubarak Ibrahim</h1>
+              <p className="text-[14px]">Admin</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 p-10">{<Outlet />}</div>
+      </div>
+    </div>
+  );
+}
