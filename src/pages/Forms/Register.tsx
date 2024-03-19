ed87bfc1331d516fce334ec77de4f92782ad12ab
Abdulhafiz Bindawa <abdulhafizbdw@gmail.com>
2024-03-19 01:46:14 +0100

    remove fixed form

--- src/pages/Forms/Register.tsx
+++ src/pages/Forms/Register.tsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
-import Input from '../../components/Input';
-import { InboxOutlined } from '@ant-design/icons';
+import { useEffect, useState } from "react";
+import Input from "../../components/Input";
+import { InboxOutlined } from "@ant-design/icons";
 
-import { Button, message, Modal, Spin, Upload } from 'antd';
-import PaymentSumarryModal from './components/PaymentSumarryModal';
-import { useLocation, useNavigate } from 'react-router-dom';
-import * as Yup from 'yup';
+import { Button, message, Modal, Spin, Upload } from "antd";
+import PaymentSumarryModal from "./components/PaymentSumarryModal";
+import { useLocation, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
-import { useFormik } from 'formik';
-import useRegister from './hooks/useRegister';
-import { useTranslation } from 'react-i18next';
-import AntTextArea from '../../components/TextArea';
+import { useFormik } from "formik";
+import useRegister from "./hooks/useRegister";
+import { useTranslation } from "react-i18next";
+import AntTextArea from "../../components/TextArea";
 
 const { Dragger } = Upload;
 
@@ -41,18 +41,18 @@ export default function Register() {
   const [uploading, setUploading] = useState(false);
   const [selectedNiche, setNiche] = useState<string[] | []>([]);
   const [selectedMeeting, setMeeting] = useState<string[] | []>([]);
-  const [userId, setUserId] = useState<string>('');
-  const [email, setEmail] = useState('');
+  const [userId, setUserId] = useState<string>("");
+  const [email, setEmail] = useState("");
   const { register, registering } = useRegister();
   const navigate = useNavigate();
   const location = useLocation();
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    if (phoneNumber.startsWith('+234')) {
-      i18n.changeLanguage('en');
+    if (phoneNumber.startsWith("+234")) {
+      i18n.changeLanguage("en");
     } else {
-      i18n.changeLanguage('fr');
+      i18n.changeLanguage("fr");
     }
   }, []);
   const [phoneNumber] = useState<string>(location.state.phoneNumber);
@@ -65,34 +65,34 @@ export default function Register() {
   };
 
   const validationSchema = Yup.object().shape({
-    company_name: Yup.string().required('Company name is required'),
-    creation_date: Yup.date().required('Creation date is required'),
-    address: Yup.string().required('Address is required'),
+    company_name: Yup.string().required("Company name is required"),
+    creation_date: Yup.date().required("Creation date is required"),
+    address: Yup.string().required("Address is required"),
     number_of_employees: Yup.number(),
     website: Yup.string(),
-    mobile: Yup.string().required('Mobile number is required'),
-    email: Yup.string().email('Invalid email').required('Email is required'),
+    mobile: Yup.string().required("Mobile number is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
     company_niche: Yup.array()
       .of(Yup.string())
-      .required('Company niche is required'),
+      .required("Company niche is required"),
     meeting_sectors: Yup.array().of(Yup.string()),
     import_morocco: Yup.string(),
     export_morocco: Yup.string(),
-    image_url: Yup.string().url('Invalid image URL'),
+    image_url: Yup.string().url("Invalid image URL"),
   });
   const initialValues: InitialValuesProps = {
-    company_name: '',
-    creation_date: '',
-    address: '',
-    number_of_employees: '',
-    website: '',
+    company_name: "",
+    creation_date: "",
+    address: "",
+    number_of_employees: "",
+    website: "",
     mobile: phoneNumber,
-    email: '',
+    email: "",
     company_niche: [],
-    import_morocco: '',
-    export_morocco: '',
+    import_morocco: "",
+    export_morocco: "",
     meeting_sectors: [],
-    image_url: '',
+    image_url: "",
   };
   const formik = useFormik({
     initialValues,
@@ -101,13 +101,13 @@ export default function Register() {
       if (registered.status) {
         setUserId(registered.data);
         formik.resetForm();
-        message.success('Registered successfully!');
-        if (phoneNumber.startsWith('+234')) {
+        message.success("Registered successfully!");
+        if (phoneNumber.startsWith("+234")) {
           setEmail(values.email);
 
           openSummaryModal();
         } else {
-          navigate('/');
+          navigate("/");
         }
       }
     },
@@ -122,19 +122,19 @@ export default function Register() {
     setIsModalOpen(false);
   };
   const [companyNiche] = useState([
-    'Importer/User',
-    'Wholesaler/Distributor',
-    'Intermediary',
-    'Central Purchasing',
-    'Manufacturer',
+    "Importer/User",
+    "Wholesaler/Distributor",
+    "Intermediary",
+    "Central Purchasing",
+    "Manufacturer",
   ]);
 
   const [meetingWith] = useState([
-    'Agriculture & Agro Allied',
-    'Automobile',
-    'Solid Minerals',
-    'Energy (renewable energy)',
-    'Information Technology',
+    "Agriculture & Agro Allied",
+    "Automobile",
+    "Solid Minerals",
+    "Energy (renewable energy)",
+    "Information Technology",
   ]);
   // const [selectedAvailability, setSelectedAvailabilty] = useState([]);
   // const props: UploadProps = {
@@ -157,25 +157,25 @@ export default function Register() {
   //   },
   // };
   const props: UploadProps = {
-    name: 'file',
+    name: "file",
     multiple: false,
-    action: 'https://api.cloudinary.com/v1_1/nutscoders/image/upload',
+    action: "https://api.cloudinary.com/v1_1/nutscoders/image/upload",
     beforeUpload: (file) => {
-      const uploadPreset = 'se5hxkgw'; // Replace with your Cloudinary upload preset name
+      const uploadPreset = "se5hxkgw"; // Replace with your Cloudinary upload preset name
       setUploading(true);
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('upload_preset', uploadPreset);
+      formData.append("file", file);
+      formData.append("upload_preset", uploadPreset);
 
       // Upload the file
-      fetch('https://api.cloudinary.com/v1_1/nutscoders/image/upload', {
-        method: 'POST',
+      fetch("https://api.cloudinary.com/v1_1/nutscoders/image/upload", {
+        method: "POST",
         body: formData,
       })
         .then((response) => response.json())
         .then((data) => {
           // Handle the upload response
-          console.log('Upload response:', data);
+          console.log("Upload response:", data);
           if (data.error) {
             message.error(`${file.name} upload failed: ${data.error.message}`);
           } else {
@@ -185,7 +185,7 @@ export default function Register() {
           }
         })
         .catch((error) => {
-          console.error('Upload error:', error);
+          console.error("Upload error:", error);
           message.error(`${file.name} upload failed.`);
         });
       setUploading(false);
@@ -194,39 +194,41 @@ export default function Register() {
     },
 
     onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
+      console.log("Dropped files", e.dataTransfer.files);
     },
   };
 
   return (
     <div
       // style={{ backgroundImage: 'url(rectangle.png)' }}
-      className="bg-cover bg-center h-[100vh] w-full relative overflow-x-hidden">
+      className="bg-cover bg-center h-[100vh] w-full relative overflow-x-hidden"
+    >
       <div
         // style={{
         //   backgroundImage: 'url(round.png)',
         //   backgroundRepeat: 'no-repeat',
         // }}
-        className="fixed top-0 bottom-0 right-0 left-0 bg-bgImage  bg-contain bg-center flex flex-col items-center p-5 md:p-11 overflow-x-hidden">
-        <h1 className="text-lightGreen font-bold text-4xl">{t('Register')}</h1>
-        <span>{t('Part')}</span>
-        <p className="text-2xl"> {t('Fill')}</p>
+        className="top-0 bottom-0 right-0 left-0 bg-bgImage  bg-contain bg-center flex flex-col items-center p-5 md:p-11 overflow-x-hidden"
+      >
+        <h1 className="text-lightGreen font-bold text-4xl">{t("Register")}</h1>
+        <span>{t("Part")}</span>
+        <p className="text-2xl"> {t("Fill")}</p>
 
         <div className="flex-1 flex-col bg-transparent min-h-[200px] w-full md:w-[70%] mt-5 overflow-x-hidden">
-          <span className="text-[18px] text-lightGreen">{t('Personal')}</span>
+          <span className="text-[18px] text-lightGreen">{t("Personal")}</span>
           <div className="flex flex-col md:flex-row gap-0 md:gap-3 items-center">
             <Input
               error={
                 formik.touched.company_name && formik.errors.company_name
                   ? formik.errors.company_name
-                  : ''
+                  : ""
               }
               value={formik.values.company_name}
               onChange={formik.handleChange}
               id="company_name"
               className="w-full md:w-[70%]"
               required
-              label={t('Name')}
+              label={t("Name")}
               placeholder="Enter the name of the company"
               outlined={false}
             />
@@ -235,14 +237,14 @@ export default function Register() {
               error={
                 formik.touched.creation_date && formik.errors.creation_date
                   ? formik.errors.creation_date
-                  : ''
+                  : ""
               }
               value={formik.values.creation_date}
               id="creation_date"
               onChange={formik.handleChange}
               className="w-full md:w-[30%]"
               required
-              label={t('Creation')}
+              label={t("Creation")}
               placeholder="Enter the name of the company"
               outlined={false}
               type="date"
@@ -253,13 +255,13 @@ export default function Register() {
             error={
               formik.touched.address && formik.errors.address
                 ? formik.errors.address
-                : ''
+                : ""
             }
             value={formik.values.address}
             onChange={formik.handleChange}
             required
             id="address"
-            label={t('Address')}
+            label={t("Address")}
             outlined={false}
             placeholder="Type your address"
           />
@@ -268,12 +270,12 @@ export default function Register() {
               error={
                 formik.touched.website && formik.errors.website
                   ? formik.errors.website
-                  : ''
+                  : ""
               }
               value={formik.values.website}
               onChange={formik.handleChange}
               className="w-full md:w-[70%]"
-              label={t('Website')}
+              label={t("Website")}
               id="website"
               placeholder="www.example.com"
               outlined={false}
@@ -285,12 +287,12 @@ export default function Register() {
                 formik.touched.number_of_employees &&
                 formik.errors.number_of_employees
                   ? formik.errors.number_of_employees
-                  : ''
+                  : ""
               }
               value={formik.values.number_of_employees}
               onChange={formik.handleChange}
               className="w-full md:w-[30%]"
-              label={t('Employees')}
+              label={t("Employees")}
               outlined={false}
               type="number"
             />
@@ -302,7 +304,7 @@ export default function Register() {
               value={phoneNumber}
               disabled
               className="w-full md:w-[30%]"
-              label={t('Mobile')}
+              label={t("Mobile")}
               outlined={false}
             />
 
@@ -311,18 +313,18 @@ export default function Register() {
               error={
                 formik.touched.email && formik.errors.email
                   ? formik.errors.email
-                  : ''
+                  : ""
               }
               value={formik.values.email}
               id="email"
               onChange={formik.handleChange}
               className="w-full md:w-[70%]"
-              label={t('Email')}
+              label={t("Email")}
               outlined={false}
             />
           </div>
           <h1 className="my-[10px] text-[13px] font-[500]">
-            {t('Is')}
+            {t("Is")}
             <span className="text-[red]">*</span>
           </h1>
           {companyNiche.map((options, ind) => (
@@ -349,12 +351,12 @@ export default function Register() {
             error={
               formik.touched.import_morocco && formik.errors.import_morocco
                 ? formik.errors.import_morocco
-                : ''
+                : ""
             }
             value={formik.values.import_morocco}
             id="import_morocco"
             onChange={formik.handleChange}
-            label={t('What')}
+            label={t("What")}
             outlined={false}
             placeholder="Type here"
           />
@@ -363,17 +365,17 @@ export default function Register() {
             error={
               formik.touched.export_morocco && formik.errors.export_morocco
                 ? formik.errors.export_morocco
-                : ''
+                : ""
             }
             value={formik.values.export_morocco}
             id="export_morocco"
             onChange={formik.handleChange}
             className="mb-10"
-            label={t('Export')}
+            label={t("Export")}
             outlined={false}
             placeholder="Type here"
           />
-          <span className="font-[500]">{t('Meeting')}</span>
+          <span className="font-[500]">{t("Meeting")}</span>
           {meetingWith.map((options, ind) => (
             <div className="my-3 flex items-center" key={ind.toString()}>
               <input
@@ -394,7 +396,7 @@ export default function Register() {
             </div>
           ))}
 
-          <span className="mt-5">{t('Passport')}</span>
+          <span className="mt-5">{t("Passport")}</span>
           <div className="h-[200px] md:h-[150px] mt-4">
             <Dragger showUploadList={true} className="h-[150px]" {...props}>
               <p className="ant-upload-drag-icon">
@@ -403,7 +405,7 @@ export default function Register() {
 
               <p className="ant-upload-text">
                 {uploading && <Spin size="large" spinning />}
-                {t('Drag')}
+                {t("Drag")}
               </p>
               <p className="ant-upload-hint">
                 Please Upload or Capture your passport
@@ -424,10 +426,11 @@ export default function Register() {
                 formik.handleSubmit();
               }}
               className="bg-lightGreen h-[38px]"
-              type="primary">
-              {phoneNumber.startsWith('+234')
-                ? 'Submit & proceed to payment'
-                : t('Submit')}
+              type="primary"
+            >
+              {phoneNumber.startsWith("+234")
+                ? "Submit & proceed to payment"
+                : t("Submit")}
             </Button>
           </div>
         </div>
@@ -440,7 +443,8 @@ export default function Register() {
         centered
         title=""
         open={isModalOpen}
-        onOk={handleOk}>
+        onOk={handleOk}
+      >
         <div className="flex flex-col min-h-[400px] bg-white  items-cente p-3">
           <h1 className="text-[16px] text-black">Terms and Conditions</h1>
           <div className="border-[#9D9DB7] border h-[217px] w-full my-5 overflow-y-scroll p-[10px]">
@@ -554,13 +558,15 @@ export default function Register() {
           <div className="flex gap-4 items-center justify-end mt-5">
             <Button
               onClick={handleCancel}
-              className="border-lightGreen bg-transparent text-lightGreen h-[38px]">
+              className="border-lightGreen bg-transparent text-lightGreen h-[38px]"
+            >
               Cancel
             </Button>
             <Button
               onClick={handleCancel}
               className="bg-lightGreen h-[38px]"
-              type="primary">
+              type="primary"
+            >
               Confirm
             </Button>
           </div>
